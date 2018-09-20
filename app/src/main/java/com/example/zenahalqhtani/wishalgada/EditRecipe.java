615c827ce2911079741a848026d97831db3e7acc
zyoonn18 <zenah.alqhtani@gmail.com>
2018-09-20 23:43:55 +0300

    four page

---
+++ app/src/main/java/com/example/zenahalqhtani/wishalgada/EditRecipe.java
@@ -0,0 +1,13 @@
+package com.example.zenahalqhtani.wishalgada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_recipe);
+    }
+}
